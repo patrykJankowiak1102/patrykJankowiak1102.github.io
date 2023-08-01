e1e5b99daa089a3d605d282f48864761e3c950f7
patrykJankowiak1102 <patryk.jankowiak.1102@gmail.com>
2023-08-01 22:57:26 +0200

    alert odpalany tylko co 10 raz

--- js/script.js
+++ js/script.js
@@ -1,4 +1,30 @@
 window.onload = setTimeout(() => {
-    
-    alert("Strona jeszcze nie skończona, mogą występować liczne błędy i niedociągnięcia:)");
+
+    let alertCheck = localStorage.getItem("alert");
+
+    if (alertCheck == undefined) {
+
+        alertCheck = 0;
+
+        localStorage.setItem("alert", alertCheck);
+
+    } else {
+
+        if (alertCheck < 10) {
+
+            alertCheck++;
+
+            localStorage.setItem("alert", alertCheck);
+
+            console.log(alertCheck);
+
+        } else {
+
+            alert("Strona jeszcze nie skończona, mogą występować liczne błędy i niedociągnięcia:)");
+
+            alertCheck = 0;
+
+            localStorage.setItem("alert", 0);
+        }
+    }
 }, 1000);
